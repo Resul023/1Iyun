20a7226c6c4d521c1e4d815488fb4e7b105747fb
Resul023 <73m6u7h@code.edu.az>
2022-06-01 02:18:09 -0700

    HomeWork

---
+++ Practice/Practice/Controllers/PortfolioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practice.DAL;
+using Practice.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PortfolioController(AppDbContext context )
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var portfolios = _context.Portfolios.ToList();
+            var category = _context.Categories.ToList();
+            PortfolioView newPort = new PortfolioView
+            {
+            
+                Portfolios = portfolios,
+                Category = category
+            };
+            return View(newPort);
+        }
+    }
+}
---
+++ Practice/Practice/ViewModels/PortfolioView.cs
@@ -0,0 +1,14 @@
+﻿using Practice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice.ViewModels
+{
+    public class PortfolioView
+    {
+        public List<Categories> Category { get; set; }
+        public List<Portfolio> Portfolios { get; set; }
+    }
+}
